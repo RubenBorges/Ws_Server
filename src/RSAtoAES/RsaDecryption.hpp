78be9b70812b5af33a95e9aa5d601fe791f7e82c
RubenBorges <Ruben_Borges711@hotmail.com>
2026-08-01 22:25:57 -0400

    added the beginnings of a Secure RSA -> AES communication program

--- src/RSAtoAES/RsaDecryption.hpp
+++
@@ -1,8 +0,0 @@
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
-#include <filesystem>
-#include <vector>
-namespace crypt {
-std::optional<std::string>  RsaDecryption(std::filesystem::path privatekey,std::vector<unsigned char>& cipherText );
-}
